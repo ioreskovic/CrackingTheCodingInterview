b07f9422b2e85a81a5e7b54c72ed4ad057afdc68
eivaore <ivan.oreskovic@ericsson.com>
2014-11-21 09:58:23 +0100

    Cleaned code for EulerianCycle, added algorithm comments step-by-step and another true positive test case

--- src/com/lopina/exercises/chapter4/interview/EulerianGraph.java
+++ src/com/lopina/exercises/chapter4/interview/EulerianGraph.java
@@ -10,6 +10,14 @@ import com.lopina.exercises.chapter4.graphs.Graph;
 
 import edu.princeton.cs.introcs.In;
 
+/**
+ * Finds an Eulerian cycle in a graph.</br>
+ * An Eulerian cycle is a sequence of visits to graph vertices in such a way that<br>
+ * each edge is visited only once and that the start node is the end node aswell.</br>
+ * 
+ * @author eivaore
+ *
+ */
 public class EulerianGraph {
 
 	private class Edge {
@@ -86,11 +94,21 @@ public class EulerianGraph {
 		}
 	}
 
+	/**
+	 * Checks if the specidied graph meets the criteria of Eulerian cycle.<br>
+	 * In order for a graph to have Eulerian cycle, all of its vertices need to</br>
+	 * have even number of edges connected to them.</br>
+	 * 
+	 * @param graph the specified graph
+	 * @return <code>true</code> if the graph has Eulerian cycle, <code>false</code> otherwise
+	 */
 	@SuppressWarnings("unchecked")
 	private boolean hasEulerianCycle(Graph graph) {
 		edgesMap = new HashMap<Edge, Integer>();
 		adj = (Iterator<Integer>[]) new Iterator[graph.V()];
 		
+		// Collect data about vertices and corresponding edges
+		// Runs in O(V + E) time 
 		for (int v = 0; v < graph.V(); v++) {
 			adj[v] = graph.adj(v).iterator();
 			if (graph.adj(v).size() % 2 == 1) {
@@ -122,54 +140,64 @@ public class EulerianGraph {
 	}
 
 	/**
-	 * See: <a href="http://iampandiyan.blogspot.in/2013/10/c-program-to-find-euler-path-or-euler.html">http://iampandiyan.blogspot.in/2013/10/c-program-to-find-euler-path-or-euler.html</a>
-	 * @param graph
+	 * See: <a href="http://iampandiyan.blogspot.in/2013/10/c-program-to-find-euler-path-or-euler.html">http://iampandiyan.blogspot.in/2013/10/c-program-to-find-euler-path-or-euler.html</a></br>
+	 * Constructs Eulerian cycle for the given graph</br>
+	 * Time complexity is O(E + V)
+	 * 
+	 * @param graph the specified graph
 	 */
 	private void findEulerianCycle(Graph graph) {
 		eulerianCycle = new ArrayDeque<Integer>();
 		Deque<Integer> stack = new ArrayDeque<Integer>();
+		
+		// Push the initial vertex to the stack
 		stack.offerFirst(0);
 
+		// While there is something on the stack, we can try exploring further in a DFS manner
 		while (!stack.isEmpty()) {
+			// Check which vertex is the most recent one to explore
 			int v = stack.peek();
-			System.out.println("Examining top of temp stack: " + v);
 			
+			// For each of its neighboring vertices
 			while (adj[v].hasNext()) {
+				// Check if we have traversed that edge before
 				int w = adj[v].next();
-				System.out.println("\tNext possible vertex is: " + w);
-				
 				Edge edge = new Edge(v, w);
 				
+				// If we can still use it it
 				if (edgeAvailable(edge)) {
-					System.out.println("\t\tEdge from " + v + " to " + w + " is free");
+					// Push the other end of that edge onto the stack and mark that we've used it
 					stack.offerFirst(w);
 					decrementEdgeCount(edge);
+					
+					// set the current vertex to explore for children the other vertex of the edge
 					v = w;
-					continue;
-				} else {
-					System.out.println("\t\tEdge from " + v + " to " + w + " is not free");
-//					break;
 				}
 			}
 			
-			System.out.println();
-			System.out.println("Trying to find the place to re-loop");
-			
+			// We've come to a place where our current node has no more available edges
+			// So we need to backtrack to find possible loops we missed
+			// While there is something on the stack
 			while (!stack.isEmpty()) {
+				
+				// Take the latest vertex and push it on another stack to keep traversal order
 				int x = stack.pop();
 				eulerianCycle.push(x);
-				System.out.println("\tMoving " + x + " to cycle stack");
-				
+
+				// If we've come to the end of the stack, we've completed the search
+				// and the traversal order is on the another stack
 				if (stack.isEmpty()) {
-					System.out.println("\tCycle done");
 					return;
 				}
 				
+				// If there is something on the stack
+				// Check what it is
 				int k = stack.peek();
-				System.out.println("\tBacktracking from " + k);
 				
+				// If that something still has unexplored edges
+				// Set it as the node to be examined and force break the loop
+				// And start outer one again from that point
 				if (adj[k].hasNext()) {
-					v = k;
 					break;
 				}
 			}
@@ -197,6 +225,9 @@ public class EulerianGraph {
 		
 		EulerianGraph egBig = new EulerianGraph(new Graph(new In("eulerianBig.txt")));
 		System.out.println(egBig.getEulerianCycle());
+		
+		EulerianGraph egEnv = new EulerianGraph(new Graph(new In("eulerianEnvelope.txt")));
+		System.out.println(egEnv.getEulerianCycle());
 	}
 	
 }
