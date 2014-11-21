e0efa23199b30db73b80db66be43413811aad68d
eivaore <ivan.oreskovic@ericsson.com>
2014-11-21 11:03:27 +0100

    Added Digraph class which represents Directed graph

---
+++ src/com/lopina/exercises/chapter4/graphs/Digraph.java
@@ -0,0 +1,114 @@
+package com.lopina.exercises.chapter4.graphs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.introcs.In;
+
+public class Digraph {
+	private final int V;
+	private int E;
+	private List<Integer>[] adj;
+	
+	@SuppressWarnings("unchecked")
+	public Digraph(int V) {
+		super();
+		this.V = V;
+		this.E = 0;
+		this.adj = (List<Integer>[]) new List[V];
+		
+		for (int v = 0; v < V; v++) {
+			adj[v] = new ArrayList<Integer>();
+		}
+	}
+	
+	public Digraph(In in) {
+        try {
+            this.V = in.readInt();
+            if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+            adj = (List<Integer>[]) new List[V];
+            for (int v = 0; v < V; v++) {
+                adj[v] = new ArrayList<Integer>();
+            }
+            int E = in.readInt();
+            if (E < 0) throw new IllegalArgumentException("Number of edges in a Digraph must be nonnegative");
+            for (int i = 0; i < E; i++) {
+                int v = in.readInt();
+                int w = in.readInt();
+                addEdge(v, w); 
+            }
+        }
+        catch (NoSuchElementException e) {
+            throw new InputMismatchException("Invalid input format in Digraph constructor");
+        }
+    }
+	
+	public Digraph(Digraph G) {
+        this(G.V());
+        this.E = G.E();
+        for (int v = 0; v < G.V(); v++) {
+            // reverse so that adjacency list is in same order as original
+            Deque<Integer> reverse = new ArrayDeque<Integer>();
+            for (int w : G.adj[v]) {
+                reverse.push(w);
+            }
+            for (int w : reverse) {
+                adj[v].add(w);
+            }
+        }
+    }
+	
+	public int V() {
+        return V;
+    }
+	
+	public int E() {
+        return E;
+    }
+	
+	private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+	
+	public void addEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(w);
+        E++;
+    }
+	
+	public Iterable<Integer> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+	
+	public Digraph reverse() {
+        Digraph R = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj(v)) {
+                R.addEdge(w, v);
+            }
+        }
+        return R;
+    }
+	
+	@Override
+	public String toString() {
+        StringBuilder s = new StringBuilder();
+        String NEWLINE = System.getProperty("line.separator");
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(String.format("%d: ", v));
+            for (int w : adj[v]) {
+                s.append(String.format("%d ", w));
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+}
